8dea324d4f8a3ee3a5888fd1b36445e7da70400d
ohohoi <ohohoi@sookmyung.ac.kr>
2020-08-28 22:35:50 +0900

    create thymeleaf view

--- src/main/java/com/example/cookie/controller/CookieController.java
+++ src/main/java/com/example/cookie/controller/CookieController.java
@@ -1,12 +1,14 @@
 package com.example.cookie.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class CookieController {
-    @GetMapping("/")
-    public String cookies() {
-        return "";
+    @GetMapping("/get/cookies")
+    public String cookies(Model model) {
+
+        return "cookies.html";
     }
 }
